d87ec3b8778bff4d806d7db444369cb25762f3ff
dahuoyzs <835476090@qq.com>
2020-05-30 09:04:20 +0800

    add

--- autocommit.sh
+++ autocommit.sh
@@ -7,10 +7,11 @@ cd /opt/shell/autocommit/greengit/
 iscommit=`cat config.txt|grep ${today}|awk '{print $2}'`
 if [ $iscommit == 1 ] ;
 then 
-sentence=`curl 127.0.0.1:81/api/sentence`
-echo `date +"%Y-%m-%d_%H:%M:%S"`$sentence >> /opt/shell/autocommit/greengit/README.md
+#sentence=`curl 127.0.0.1:81/api/sentence`
+#echo `date +"%Y-%m-%d_%H:%M:%S"`$sentence >> /opt/shell/autocommit/greengit/README.md
+echo `date +"%Y-%m-%d_%H:%M:%S"`>> /opt/shell/autocommit/greengit/README.md
 git add README.md
 git commit -m "by bigfire autocommit program"
-git push origin  master
+git push origin  master -f
 fi
 
